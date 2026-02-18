09fe0951647620003e58a3f787def53fc0a76708
ML <louism0495@gmail.com>
2026-02-18 10:37:31 +0100

    add new def of iter func to accept const

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: mlouis <mlouis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 16:04:58 by mlouis            #+#    #+#             */
-/*   Updated: 2026/02/17 17:16:07 by mlouis           ###   ########.fr       */
+/*   Updated: 2026/02/18 10:34:32 by mlouis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define ITER_HPP
 
 # include <cstddef>
-
+# include <iostream>
 template<typename T>
 void	iter(T* arr, const std::size_t len, void (*func)(T&))
 {
@@ -27,24 +27,16 @@ void	iter(T* arr, const std::size_t len, void (*func)(T&))
 	}
 }
 
-// template<typename T>
-// void	iter(T* arr, const std::size_t len, void (*func)(T&))
-// {
-// 	if (!arr || !func)
-// 		return ;
+template<typename T>
+void	iter(const T* arr, const std::size_t len, void (*func)(const T&))
+{
+	if (!arr || !func)
+		return ;
 	
-// 	for (std::size_t i = 0 ; i < len ; ++i)
-// 		func(arr[i]);
-// }
-
-// template<typename T>
-// void	iter(T* arr, const std::size_t len, void (*func)(const T&))
-// {
-// 	if (!arr || !func)
-// 		return ;
-	
-// 	for (std::size_t i = 0 ; i < len ; ++i)
-// 		func(arr[i]);
-// }
+	for (std::size_t i = 0 ; i < len ; ++i)
+	{
+		func(arr[i]);
+	}
+}
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mlouis <mlouis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 16:04:52 by mlouis            #+#    #+#             */
-/*   Updated: 2026/02/17 17:16:51 by mlouis           ###   ########.fr       */
+/*   Updated: 2026/02/18 10:33:56 by mlouis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,25 @@
 template<typename T>
 void	plusOne(T& t)
 {
-	t = t + 1;
+	t += 1;
 }
 
 void	plusOne(std::string& t)
 {
-	t.append("1");
+	t += "1";
 }
 
 template<typename T>
-void	print(T& t)
+void	print(const T& t)
 {
 	std::cout << "'" << t << "' ";
 }
 
 int main()
 { 
-	//? Normal arrays
-	// int			arrI[3] = {2, 9, 14};
-	// char		arrC[4] = {'b', 'o', 'o', 'm'};
-	// std::string	arrS[2] = {"hello", "world"};
-
-	//? Non complete arrays
-	// int			arrI[3] = {2, 9};
-	// char		arrC[4] = {'b', 'o', 'o'};
-	// std::string	arrS[2] = {"hello"};
-
-	//? 
-	// int			arrI[3] = {2, 0, 9};
-	// char		arrC[4] = {'b', 0,'o', 'o'};
-	// std::string	arrS[2] = {0, "hello"};
-
-	int			arrI[3];
-	char		arrC[4];
-	std::string	arrS[2];
+	int			arrI[3] = {2, 9, 14};
+	char		arrC[4] = {'b', 'o', 'o', 'm'};
+	std::string	arrS[2] = {"hello", "world"};
 
 	iter(arrI, 3, print);
 	std::cout << std::endl;
@@ -69,5 +54,17 @@ int main()
 	std::cout << std::endl;
 	iter(arrS, 2, print);
 	std::cout << std::endl;
+
+	const int			cArrI[3] = {63, 11, 83};
+	const char			cArrC[4] = {'y', 'o', 'p', '!'};
+	const std::string	cArrS[2] = {"potato", "banana"};
+
+	iter(cArrI, 3, print);
+	std::cout << std::endl;
+	iter(cArrC, 4, print);
+	std::cout << std::endl;
+	iter(cArrS, 2, print);
+	std::cout << std::endl;
+
 	return (0);
 }
